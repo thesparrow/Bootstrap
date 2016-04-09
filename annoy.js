a951be5760bc315a65412726477ffe8de33da8d4
Anna <Anya@Annas-MacBook-Air.local>
2016-04-08 18:26:09 -0700

    clean up

--- annoy.js
+++ annoy.js
@@ -2,19 +2,6 @@
 
 var answer = prompt("Are we there yet?");
 
-/*if (answer === "yes"){
-	alert("We made it!");
-}else {
-	var answer = prompt("Are we there yet?");
-}*/
-
-// while (answer !== "yes" && answer !== "ya") {
-//     var answer = prompt("Are we there yet?")
-// }
-
-// alert("YAY! WE MADE IT!!");
-
-
 while (answer.indexOf("yes") == -1 && answer.indexOf("ya") == -1) {
     var answer = prompt("Are we there yet?")
 }
